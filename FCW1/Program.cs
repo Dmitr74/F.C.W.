b218c8e6ea7358852ed7393d673ba5eed1200ce2
Dmitrii Lih <omts1dm@gmail.com>
2023-04-10 23:35:10 +0500

    2

--- FCW1/Program.cs
+++ FCW1/Program.cs
@@ -6,3 +6,16 @@
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
+
+Console.Write("Введите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Критерий поиска, количество символов: ");
+int value = Convert.ToInt32(Console.ReadLine());
+string[] arr = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-й элемент массива: ");
+    string? str = Convert.ToString(Console.ReadLine());
+    arr[i] = str!;
+}
+
